3eaa49fc36d080a67b2ade38bb7b66c646ec92e3
jasdevsidhu12 <jasdev.project.12@gmail.com>
2018-09-23 17:21:27 +0100

    finished project implementation II

--- src/app/project-body/project-body-item/projects/FEProj3.jsx
+++ src/app/project-body/project-body-item/projects/FEProj3.jsx
@@ -5,7 +5,7 @@ import { Typography } from '@material-ui/core';
 const FEPro3 = () => {
   return (
     <Typography>
-      <b>Front-End, “BIMRooms” noticeboard prototype for IBMers <a href="https://github.com/jasdevsidhu12/BIMRooms">(github.com/jasdevsidhu12/BIMRooms)</a></b>
+      <b>Front-End, “BIMRooms” noticeboard prototype for IBMers <a href="https://github.com/jasdevsidhu12/BIMRooms" target="_blank">(github.com/jasdevsidhu12/BIMRooms)</a></b>
       <ul>
         <li>A prototype ‘noticeboard’ UI potentially replace to traditional old notice board at IBM offices.</li>
         <li>Technologies used are ReactJS, Webpack, Babel, ES6, Express(NodeJS), SASS (CSS, CSS3).</li>
